12130e0af8193f0412c0ecb2d2b634cb201a72bf
kristine <codewithtine@gmail.com>
2020-10-31 08:37:23 -0400

    Before	starting the	implementation

---
+++ MaryKristineTestA/src/main/java/marykristine/pacleb/s301082263/MaryKristineActivity.java
@@ -0,0 +1,16 @@
+package marykristine.pacleb.s301082263;
+
+//Mary Kristine Pacleb | 301082263
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaryKristineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
